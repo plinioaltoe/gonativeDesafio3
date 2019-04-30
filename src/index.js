102cac1b31f660569b10450c3f31553e9c893ea6
plinioaltoe <plinioaltoe@yahoo.com.br>
2019-04-29 23:47:08 -0300

    primeiro commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "~/config/ReactotronConfig";
+import "~/config/DevToolsConfig";
+import React from "react";
+import { View } from "react-native";
+// import Mapa from "~/components/Mapa";
+
+const App = () => <View />;
+
+export default App;
